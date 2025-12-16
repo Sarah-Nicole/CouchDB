8e66c4bd70e197b0006d1070afdea6b13adfb771
Sarah Kolb <sarah.n.kolb@gmail.com>
2025-12-16 11:35:25 +0100

    Version-1

---
+++ CouchDbDemo/CoffeeConsumption.cs
@@ -0,0 +1,27 @@
+﻿using CouchDB.Driver.Types;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CouchDbDemo
+{
+    // muss von CouchDocument erben (für _id und _rev)
+    public class CoffeeConsumption : CouchDocument
+    {
+        [JsonProperty("employeeName")]
+        public string? EmployeeName { get; set; }
+
+        [JsonProperty("cupsPerDay")]
+        public int CupsPerDay { get; set; }
+
+        [JsonProperty("isStillAlive")]
+        public bool IsStillAlive { get; set; }
+
+        [JsonProperty("lastCupTime")]
+        public DateTime LastCupTime { get; set; }
+    }
+}
---
+++ CouchDbDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using CouchDbDemo;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var client = new CouchDbClient();
+
+        // -------------------- Alle anzeigen --------------------
+        var all = await client.GetAllAsync();
+        Console.WriteLine("Alle Dokumente:");
+        all.ForEach(x => Console.WriteLine($"{x.EmployeeName} - {x.CupsPerDay} Tassen"));
+
+        // -------------------- Nach Name suchen --------------------
+        var sarahs = await client.GetByNameAsync("Sarah");
+        Console.WriteLine("Mitarbeiter mit 'Sarah':");
+        sarahs.ForEach(x => Console.WriteLine($"{x.EmployeeName} - {x.CupsPerDay} Tassen"));
+
+        // -------------------- Neuen Datensatz anlegen --------------------
+        var newCoffee = new CoffeeConsumption
+        {
+            EmployeeName = "Maya",
+            CupsPerDay = 5,
+            IsStillAlive = true,
+            LastCupTime = DateTime.UtcNow
+        };
+
+        await client.AddAsync(newCoffee);
+        Console.WriteLine("Neuer Datensatz angelegt.");
+
+        // -------------------- Datensatz ändern --------------------
+        newCoffee.CupsPerDay = 7;
+        newCoffee.LastCupTime = DateTime.UtcNow;
+        await client.UpdateAsync(newCoffee);
+        Console.WriteLine("Datensatz aktualisiert.");
+
+        // -------------------- Datensatz löschen --------------------
+        await client.DeleteAsync(newCoffee);
+        Console.WriteLine("Datensatz gelöscht.");
+    }
+}
